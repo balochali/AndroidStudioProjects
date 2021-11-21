05d1e3b4409998b934d62db758a431cd24fec782
balochali <ali.albaloshi2000@gmail.com>
2021-11-21 15:06:29 +0500

    GuessingGame

--- GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.java
+++ GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.guessinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name = findViewById(R.id.editName);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnClick(View v){
+        Intent intent = new Intent(this, GamingActivity.class);
+        intent.putExtra("Name", name.getText().toString());
+        startActivity(intent);
     }
 }
