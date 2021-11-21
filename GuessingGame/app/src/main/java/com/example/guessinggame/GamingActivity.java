3094e038ab1f7db1e751e463f94a22512ce83f50
balochali <ali.albaloshi2000@gmail.com>
2021-11-21 14:58:37 +0500

    GuessingGame

---
+++ GuessingGame/app/src/main/java/com/example/guessinggame/GamingActivity.java
@@ -0,0 +1,14 @@
+package com.example.guessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gaming);
+    }
+}
