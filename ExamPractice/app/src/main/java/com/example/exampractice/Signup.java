313ad39f92a29be779ec3942a12bd09bcc6600f5
balochali <ali.albaloshi2000@gmail.com>
2021-11-23 09:39:52 +0500

    Android Exam Practice

--- ExamPractice/app/src/main/java/com/example/exampractice/Signup.java
+++ ExamPractice/app/src/main/java/com/example/exampractice/Signup.java
@@ -1,7 +1,10 @@
 package com.example.exampractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+
+    public void Login(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
