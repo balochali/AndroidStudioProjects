15837100354d09291a3b73d56a5c6adb0ca7316e
balochali <ali.albaloshi2000@gmail.com>
2021-11-28 17:54:06 +0500

    LabExamTask

--- labExam/app/src/main/java/com/example/labexam/MainActivity.java
+++ labExam/app/src/main/java/com/example/labexam/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.labexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText PlayerOneName;
+    EditText PlayerTwoName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Play(View v){
+        PlayerOneName = findViewById(R.id.EditPlayer1);
+        PlayerTwoName = findViewById(R.id.EditPlayer2);
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("PlayerOneName", PlayerOneName.getText().toString());
+        intent.putExtra("PlayerTwoName", PlayerTwoName.getText().toString());
+
+        startActivity(intent);
     }
 }
--- labExam/app/src/main/java/com/example/labexam/MainActivity2.java
+++ labExam/app/src/main/java/com/example/labexam/MainActivity2.java
@@ -2,13 +2,43 @@ package com.example.labexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView PlayerOneScore;
+    TextView PlayerTwoScore;
+
+    boolean PlayerOneCheck;
+    boolean PlayerTwoCheck;
+
+    String PlayerOneName;
+    String PlayerTwoName;
+
+    int PlayerOneCount;
+    int PlayerTwoCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        PlayerOneName = intent.getStringExtra("PlayerOneName");
+        PlayerTwoName = intent.getStringExtra("PlayerTwoName");
+
+        PlayerOneScore = findViewById(R.id.PlayerOneScore);
+        PlayerTwoScore = findViewById(R.id.PlayerTwoScore);
+
+        PlayerOneScore.setText(PlayerOneName + "Score: ");
+        PlayerTwoScore.setText(PlayerTwoName + "Score: ");
+
+    }
+
+    public void onHit(View v){
+
     }
 }
